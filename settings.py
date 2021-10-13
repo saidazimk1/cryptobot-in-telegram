72d7bbe841e0b1e2ab204d73f2a96623b35fb8dd
Sayidazim <92174111+saidazimk1@users.noreply.github.com>
2021-10-13 05:37:53 +0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 ﻿TICKETS_FN = r'Data/tickets.txt'  # файл со списком валют для проверки
 CONFIG_FN = r'Data/config.txt'  # файл со списком настроек
 
-BOT_TOKEN = '2087601416:AAF6IdQkZCzEBLvC_wcO2N1_sNbBfV2IDZ4'  # получить у @BotFather
+BOT_TOKEN = 'API KEY'  # получить у @BotFather
 CHAT_ID = -1001769355997  # куда отправлять уведомление. чтобы узнать: переслать любое сообщение боту @getmyid_bot
